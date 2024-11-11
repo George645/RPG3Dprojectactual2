110ab59e1c1a24d5e5934cb67f5f7ac3c99ef970
George645 <gsvbutterfly@outlook.com>
2024-11-11 16:21:03 +0000

    Initial commit

---
+++ Assets/Player/Player movement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    int level = 0, experience = 0;
+    [SerializeField] Rigidbody rb;
+    private Vector3 movement = new();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
